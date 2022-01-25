ff9b0dc188354518a0a044bb710ea71b87ac10c4
Emily Nozaki <emiri3128.gmail.com>
2022-01-25 18:06:43 +0900

    rendaアプリ。丸ボタンかわいい。

--- renda/ViewController.swift
+++ renda/ViewController.swift
@@ -8,11 +8,26 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var countLabel: UILabel!
+    
+    @IBOutlet weak var tapButton: UIButton!
+    
+    var number: Int = 0
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        tapButton.layer.cornerRadius = 125
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func buttonTapped(_ sender: Any) {
+        number += 1
+        countLabel.text = String(number)
+    }
+    
 
 
 }
